364211f2c9aff78fffc780e44093702f1f21083f
Oscar <oesoto@MacBook-Pro-Touch.local>
2018-02-10 14:58:26 -0500

    inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCc8JxsmwAyhKIGMewsewdTIhSIBKXdYRg",
+    authDomain: "angularlabfirebase.firebaseapp.com",
+    databaseURL: "https://angularlabfirebase.firebaseio.com",
+    projectId: "angularlabfirebase",
+    storageBucket: "angularlabfirebase.appspot.com",
+    messagingSenderId: "36065790840"
+  }
 };
